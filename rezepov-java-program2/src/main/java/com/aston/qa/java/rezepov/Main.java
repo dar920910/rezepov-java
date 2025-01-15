7a2e6d11a2a713024a09b9b71dfa11293fac6dab
Denis Rezepov <me@dar920910.ru>
2025-01-15 21:11:29 +0300

    Создана стартовая реализация проекта "rezepov-java-program2".

--- rezepov-java-program2/src/main/java/com/aston/qa/java/rezepov/Main.java
+++ rezepov-java-program2/src/main/java/com/aston/qa/java/rezepov/Main.java
@@ -1,7 +1,27 @@
 package com.aston.qa.java.rezepov;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String stringsAreEqual = "Строки идентичны.";
+        final String stringsAreNotEqual = "Строки неидентичны.";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку A: ");
+        String str_A = scanner.nextLine();
+
+        System.out.print("Введите строку B: ");
+        String str_B = scanner.nextLine();
+
+        System.out.print("РЕЗУЛЬТАТ: ");
+        if (str_A.equals(str_B)) {
+            System.out.print(stringsAreEqual);
+        } else {
+            System.out.print(stringsAreNotEqual);
+        }
+
+        System.out.println();
     }
 }
