8e6fdeea708508516bbde5d0a08cfa7c186740b5
Denis Rezepov <me@dar920910.ru>
2025-01-15 21:38:30 +0300

    Создана стартовая реализация проекта "rezepov-java-program1".

--- rezepov-java-program1/src/main/java/com/aston/qa/java/rezepov/Main.java
+++ rezepov-java-program1/src/main/java/com/aston/qa/java/rezepov/Main.java
@@ -1,7 +1,31 @@
 package com.aston.qa.java.rezepov;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input an integer (A): ");
+        int number_A = scanner.nextInt();
+
+        System.out.print("Input an integer (B): ");
+        int number_B = scanner.nextInt();
+
+        System.out.print("Comparing Result: ");
+        if (number_A == number_B) {
+            System.out.println("A == B");
+        } else if (number_A > number_B) {
+            System.out.println("A > B");
+        } else {
+            System.out.println("A < B");
+        }
+
+        System.out.printf("A + B = %d\n", number_A + number_B);
+        System.out.printf("A - B = %d\n", number_A - number_B);
+        System.out.printf("A / B = %d\n", number_A / number_B);
+        System.out.printf("A * B = %d\n", number_A * number_B);
+
+        System.out.println();
     }
 }
