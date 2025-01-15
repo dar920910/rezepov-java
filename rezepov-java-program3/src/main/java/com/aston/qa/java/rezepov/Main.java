4567f51a189f3719cdfff022bd270e87ef1f6db9
Denis Rezepov <me@dar920910.ru>
2025-01-15 20:20:59 +0300

    Создана стартовая реализация проекта "rezepov-java-program3".

--- rezepov-java-program3/src/main/java/com/aston/qa/java/rezepov/Main.java
+++ rezepov-java-program3/src/main/java/com/aston/qa/java/rezepov/Main.java
@@ -2,6 +2,16 @@ package com.aston.qa.java.rezepov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int number : numbers) {
+            if (isEvenNumber(number)) {
+                System.out.println(number);
+            }
+        }
+    }
+
+    private static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
     }
 }
